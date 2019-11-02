d38fc7286ed5d1b8873f1679215cb11e830d4c9f
Rafael Nunes Bezerra Dias <rafaelnunex@hotmail.com>
2019-11-01 22:37:57 -0300

    Par de Barreiras Implementadas

--- js/flappy.js
+++ js/flappy.js
@@ -6,16 +6,43 @@ function novoElemento(tag_name, class_name) {
 }
 
 function Barreira(reversa = false) {
-    this.element = novoElemento('div', 'barreira')
+    this.elemento = novoElemento('div', 'barreira')
 
     const borda = novoElemento('div', 'borda')
     const corpo = novoElemento('div', 'corpo')
-    this.element.appendChild(reversa ? corpo: borda)
-    this.element.appendChild(reversa ? borda : corpo)
+    this.elemento.appendChild(reversa ? corpo: borda)
+    this.elemento.appendChild(reversa ? borda : corpo)
 
     this.setAltura = altura => corpo.style.height = `${altura}px`
 }
 
-const b = new Barreira(true)
-b.setAltura(200)
-document.querySelector('[wm-flappy]').appendChild(b.element)
+// const b = new Barreira(true)
+// b.setAltura(200)
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+function ParDeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento('div', 'par-de-barreiras')
+
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const altura_superior = Math.random() * (altura - abertura)
+        const altura_inferior = altura - abertura - altura_superior
+        this.superior.setAltura(altura_superior)
+        this.inferior.setAltura(altura_inferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.setX = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
+}
+
+const par_de_barreiras = new ParDeBarreiras(450, 100, 400)
+document.querySelector('[wm-flappy]').appendChild(par_de_barreiras.elemento)
